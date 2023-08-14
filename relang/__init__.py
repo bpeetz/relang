2280cf3473f595315841acfa0248dff29694fc0f
Benedikt Peetz <benedikt.peetz@b-peetz.de>
2023-08-14 11:22:42 +0200

    Fix: Update

--- relang/__init__.py
+++ relang/__init__.py
@@ -1,58 +1,5 @@
 from prompt_toolkit import prompt
-import relang
-
-
-class Relang:
-    def __init__(self, text):
-        self.ALL_POSSIBLE_DIGITS = list(map(str, [i for i in range(0, 10)]))
-        self.ALL_POSSIBLE_OPERATORS = ["-", "+", "/", "*", "^"]
-        self.text = text
-        self.cursor = 0
-
-    def peek(self):
-        if len(self.text) > 0 and self.cursor < len(self.text):
-            return self.text[self.cursor]
-        else:
-            return None
-
-    def pop(self):
-        output = self.text[self.cursor]
-        self.cursor += 1
-        return output
-
-    def expect(self, char_expected):
-        if self.peek() in char_expected:
-            return True
-        else:
-            raise ValueError(
-                f'Got {self.peek()}, but expected one of: {", ".join(char_expected)}'
-            )
-
-    def remove_whitespace(self):
-        while self.peek() == " ":
-            self.pop()
-
-    # Parse functions
-    def parse_whole_number(self):
-        self.remove_whitespace()
-        output = 0
-        while self.peek() in self.ALL_POSSIBLE_DIGITS:
-            output *= 10
-            output += int(self.pop())
-        return str(output)
-
-    def parse_operator(self):
-        self.remove_whitespace()
-        if self.expect(self.ALL_POSSIBLE_OPERATORS):
-            return self.pop()
-
-    def parse_term(self):
-        output = ""
-        while self.peek() != None:
-            output += self.parse_whole_number()
-            if self.peek() != None:
-                output += self.parse_operator()
-        return output
+from relang import Relang
 
 
 def main():
--- relang/relang.py
+++ relang/relang.py
@@ -1,21 +1,55 @@
 class Relang:
-    cursor = 0
-
     def __init__(self, text):
+        self.ALL_POSSIBLE_DIGITS = list(map(str, [i for i in range(0, 10)]))
+        self.ALL_POSSIBLE_OPERATORS = ["-", "+", "/", "*", "^"]
         self.text = text
+        self.cursor = 0
+        self.parsed_text = self.parse(text)
 
-    def peek(self):
-        return self.text[cursor]
+    def parse(self):
 
-    def pop(self):
-        output = self.text[cursor]
-        cursor += 1
+
+    def _peek(self):
+        if len(self.text) > 0 and self.cursor < len(self.text):
+            return self.text[self.cursor]
+        else:
+            return None
+
+    def _pop(self):
+        output = self.text[self.cursor]
+        self.cursor += 1
         return output
 
-    def expect(self, char_expected):
-        if self.peek() in char_expected:
-            return true
+    def _expect(self, char_expected):
+        if self._peek() in char_expected:
+            return True
         else:
             raise ValueError(
-                f'Got {self.peek()}, but expected one of: {char_expected.join(" ")}'
+                f'Got {self._peek()}, but expected one of: {", ".join(char__expected)}'
             )
+
+    def _remove_whitespace(self):
+        while self._peek() == " ":
+            self._pop()
+
+    # Parse functions
+    def _parse_whole_number(self):
+        self._remove_whitespace()
+        output = 0
+        while self._peek() in self.ALL_POSSIBLE_DIGITS:
+            output *= 10
+            output += int(self._pop())
+        return str(output)
+
+    def _parse_operator(self):
+        self._remove_whitespace()
+        if self._expect(self.ALL_POSSIBLE_OPERATORS):
+            return self._pop()
+
+    def _parse_term(self):
+        output = ""
+        while self._peek() is not None:
+            output += self._parse_whole_number()
+            if self._peek() is not None:
+                output += self._parse_operator()
+        return output
